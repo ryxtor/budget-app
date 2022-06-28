ce0bb5a9ea390bd0c6e90e42aade1138fdc83de1
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-28 16:00:27 -0300

    Add total_amount method

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,11 @@ class Group < ApplicationRecord
   has_many :group_fees, foreign_key: 'group_id', class_name: 'GroupFee', dependent: :destroy
 
   validates :name, presence: true
-  validates :name, uniqueness: true
   validates :name, length: { maximum: 50 }
   validates :icon, presence: true
+
+  def total_amount
+    fees = group_fees.map(&:fee_id)
+    fees.inject(0) { |sum, fee| sum + Fee.find(fee).amount }
+  end
 end
