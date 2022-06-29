201e1f33e394d2af6e19ce2d506aee37e1056572
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-29 17:35:51 -0300

    Fix reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
+  devise_for :users
   devise_scope :user do
     authenticated do
       root to: 'groups#index', as: 'user'
