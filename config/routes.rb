824dae2ea67d844df803ed433b74433a86876a86
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-28 22:40:43 -0300

    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   devise_scope :user do
+    authenticated do
+      root to: 'groups#index', as: 'user'
+    end
+
+    unauthenticated do
+      root to: 'menus#index', as: 'unauthenticated_user_root'
+    end
+
     get '/sign_out', to: 'devise/sessions#destroy'
   end
   
@@ -8,7 +16,6 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :fees, only: [:new, :create, :edit, :update, :destroy]
   end
-  root 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
